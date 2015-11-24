6538aff9f0a4838ac6403e3cc01bed259bef3cae
Evan Sosenko <razorx@evansosenko.com>
2015-11-23 21:29:46 -0800

    Use standard eslint npm module

--- simple-todos-react.js
+++ simple-todos-react.js
@@ -1,23 +1,25 @@
+/* eslint-env meteor */
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('counter', 0)
 
   Template.hello.helpers({
     counter: function () {
-      return Session.get('counter');
+      return Session.get('counter')
     }
-  });
+  })
 
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      Session.set('counter', Session.get('counter') + 1)
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-  });
+  })
 }
